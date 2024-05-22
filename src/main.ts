00e4ba2cc7ddbf87f405cecbf63a941ce0ad8050
sun-asterisk <sun-asterisk@sun-asterisks-MacBook-Pro.local>
2024-05-22 16:51:24 +0800

    FE Integration Adjustments: Done

--- src/main.ts
+++ src/main.ts
@@ -5,6 +5,13 @@ import { DocumentBuilder, SwaggerModule } from '@nestjs/swagger';
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
 
+  // Todo: Handle development and production separately
+  app.enableCors({
+    origin: 'http://localhost:3001',
+    methods: 'GET,HEAD,PUT,PATCH,POST,DELETE',
+    credentials: true,
+  });
+
   const config = new DocumentBuilder()
     .setTitle('API Documentation')
     .setDescription('Documentation for NestJS Playground API')
