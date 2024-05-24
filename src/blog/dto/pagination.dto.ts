c1ef1ec6490b2683e1ae9fb14c86a9f2b3772934
sun-asterisk <sun-asterisk@sun-asterisks-MacBook-Pro.local>
2024-05-24 16:23:17 +0800

    Pagination Adjustments: Done

--- src/blog/dto/pagination.dto.ts
+++ src/blog/dto/pagination.dto.ts
@@ -1,5 +1,5 @@
 import { Type } from "class-transformer"
-import { IsInt, IsNumber, IsOptional, Min } from "class-validator"
+import { IsInt, IsOptional, Min } from "class-validator"
 
 export class PaginationDto {
     @Type(() => Number)
